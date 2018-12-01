27977fc8db1883123c8c92c2eb7c0f9548db3fc5
Dave Burke <dburke84@gmail.com>
2018-12-01 07:08:41 -0600

    Implement Day01 Part1

--- src/main/kotlin/aoc/day01/Day01.kt
+++ src/main/kotlin/aoc/day01/Day01.kt
@@ -3,8 +3,21 @@ package aoc.day01
 import aoc.Day
 
 class Day01 : Day {
+
     override fun part1(input: List<String>): String {
-        return input.joinToString(separator = "\n")
+	    var result: Int = 0
+	    for (freq in input) {
+		    val sign = freq.get(0)
+		    val magnitude = freq.drop(1).toInt()
+		    if(sign == '+') {
+			    result += magnitude
+			} else if (sign == '-') {
+				result -= magnitude
+			} else {
+				throw IllegalStateException("Unknown operation $sign")
+			}
+		}
+        return result.toString()
     }
 
     override fun part2(input: List<String>): String {
