07ba0b1cda7c94026caf0cec59e034918f3aa5f5
Dave Burke <dburke84@gmail.com>
2018-12-01 08:33:28 -0600

    Add stubs for Day02

---
+++ src/main/kotlin/aoc/day02/Day02.kt
@@ -0,0 +1,15 @@
+package aoc.day02
+
+import aoc.Day
+
+class Day02 : Day {
+
+	override fun part1(input: List<String>): String {
+		TODO("not implemented")
+	}
+
+	override fun part2(input: List<String>): String {
+		TODO("not implemented")
+	}
+
+}
---
+++ src/test/kotlin/aoc/day02/Day02Test.kt
@@ -0,0 +1,17 @@
+package aoc.day02
+
+import aoc.AocTestUtil
+import org.junit.Test
+
+
+class Day02Test {
+
+	private val util: AocTestUtil = AocTestUtil(Day02())
+
+	@Test
+	fun testPart1_sample0() {
+		TODO("not implemented")
+	}
+
+}
+
