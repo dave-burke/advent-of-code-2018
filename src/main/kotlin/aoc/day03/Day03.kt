b8762e45ea373dd6056ba254bbe8b50222c44925
Dave Burke <dburke84@gmail.com>
2018-12-02 17:38:14 -0600

    Add Day03 stubs

---
+++ src/main/kotlin/aoc/day03/Day03.kt
@@ -0,0 +1,15 @@
+package aoc.day03
+
+import aoc.Day
+
+class Day03 : Day {
+
+	override fun part1(input: List<String>): String {
+		TODO("Not implemented")
+	}
+
+	override fun part2(input: List<String>): String {
+		TODO("Not implemented")
+	}
+
+}
---
+++ src/test/kotlin/aoc/day03/Day03Test.kt
@@ -0,0 +1,30 @@
+package aoc.day03
+
+import aoc.AocTestUtil
+import aoc.inputForDay
+import kotlin.test.assertEquals
+import org.junit.Test
+import org.junit.Ignore
+
+class Day03Test {
+
+	private val day = Day03()
+	private val util: AocTestUtil = AocTestUtil(day)
+
+	@Test
+	fun testPart1_sample0() {
+		util.testPart1(listOf(
+			""
+		), "")
+	}
+
+	@Test
+	@Ignore
+	fun testPart2_sample0() {
+		util.testPart2(listOf(
+			""
+		), "")
+	}
+
+}
+
