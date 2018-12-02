538e7b28dcc8f1f9335526de7a46b11a8207a622
Dave Burke <dburke84@gmail.com>
2018-12-02 08:51:03 -0600

    Implement Day02 Part1

--- src/main/kotlin/aoc/day02/Day02.kt
+++ src/main/kotlin/aoc/day02/Day02.kt
@@ -4,8 +4,32 @@ import aoc.Day
 
 class Day02 : Day {
 
+	fun charCounts(s: String): Map<Char, Int> {
+		return s.groupingBy{ it }.eachCount().toMap()
+	}
+
+	fun hasPair(counts: Map<Char, Int>): Boolean {
+		return counts.values.contains(2)
+	}
+
+	fun hasTriple(counts: Map<Char, Int>): Boolean {
+		return counts.values.contains(3)
+	}
+
+	fun countPairs(input: List<String>): Int {
+		return input.map { charCounts(it) }.count { hasPair(it) }
+	}
+
+	fun countTriples(input: List<String>): Int {
+		return input.map { charCounts(it) }.count { hasTriple(it) }
+	}
+
 	override fun part1(input: List<String>): String {
-		TODO("not implemented")
+		var nPairs = countPairs(input)
+		var nTriples = countTriples(input)
+
+		println("Found $nPairs pairs and $nTriples triples")
+		return (nPairs * nTriples).toString()
 	}
 
 	override fun part2(input: List<String>): String {
--- src/test/kotlin/aoc/day02/Day02Test.kt
+++ src/test/kotlin/aoc/day02/Day02Test.kt
@@ -1,16 +1,74 @@
 package aoc.day02
 
 import aoc.AocTestUtil
+import kotlin.test.assertEquals
 import org.junit.Test
-
+import org.junit.Ignore
 
 class Day02Test {
 
-	private val util: AocTestUtil = AocTestUtil(Day02())
+	private val day = Day02()
+	private val util: AocTestUtil = AocTestUtil(day)
+
+	@Test
+	fun testGrouping_abcdef(){
+		val result: Map<Char, Int> = day.charCounts("abcdef")
+		assertEquals(result.values.contains(2), false)
+		assertEquals(result.values.contains(3), false)
+	}
+
+	@Test
+	fun testGrouping_bababc(){
+		val result: Map<Char, Int> = day.charCounts("bababc")
+		assertEquals(result.values.contains(2), true)
+		assertEquals(result.values.contains(3), true)
+	}
+
+	@Test
+	fun testGrouping_abbcde(){
+		val result: Map<Char, Int> = day.charCounts("abbcde")
+		assertEquals(result.values.contains(2), true)
+		assertEquals(result.values.contains(3), false)
+	}
+
+	@Test
+	fun testGrouping_abcccd(){
+		val result: Map<Char, Int> = day.charCounts("abcccd")
+		assertEquals(result.values.contains(2), false)
+		assertEquals(result.values.contains(3), true)
+	}
+
+	@Test
+	fun testGrouping_aabcdd(){
+		val result: Map<Char, Int> = day.charCounts("aabcdd")
+		assertEquals(result.values.contains(2), true)
+		assertEquals(result.values.contains(3), false)
+	}
+
+	@Test
+	fun testGrouping_abcdee(){
+		val result: Map<Char, Int> = day.charCounts("abcdee")
+		assertEquals(result.values.contains(2), true)
+		assertEquals(result.values.contains(3), false)
+	}
+
+	@Test
+	fun testGrouping_ababab(){
+		val result: Map<Char, Int> = day.charCounts("ababab")
+		assertEquals(result.values.contains(2), false)
+		assertEquals(result.values.contains(3), true)
+	}
 
 	@Test
 	fun testPart1_sample0() {
-		TODO("not implemented")
+		util.testPart1(listOf(
+		"abcdef",
+		"bababc",
+		"abbcde",
+		"abcccd",
+		"aabcdd",
+		"abcdee",
+		"ababab"), "12")
 	}
 
 }
