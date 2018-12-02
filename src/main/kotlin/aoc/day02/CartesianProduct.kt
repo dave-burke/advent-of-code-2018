7d9d1a31755b95d0e950e9da7f48cfcfd9053aea
Dave Burke <dburke84@gmail.com>
2018-12-02 12:01:13 -0600

    Use loops instead of lambdas
    The initial implementation is less clear, even for a reader who
    understands flatMap, map, and lambda expressions.

--- src/main/kotlin/aoc/day02/CartesianProduct.kt
+++ src/main/kotlin/aoc/day02/CartesianProduct.kt
@@ -2,10 +2,12 @@ package aoc.day02
 
 // Extensions
 fun <T, S> Collection<T>.cartesianProduct(other: Iterable<S>): List<Pair<T, S>> {
-    return cartesianProduct(other, { first, second -> first to second })
-}
-
-fun <T, S, V> Collection<T>.cartesianProduct(other: Iterable<S>, transformer: (first: T, second: S) -> V): List<V> {
-    return this.flatMap { first -> other.map { second -> transformer.invoke(first, second) } }
+	val result: MutableList<Pair<T,S>> = mutableListOf()
+	for(a in this){
+		for(b in other){
+			result.add(a to b)
+		}
+	}
+	return result
 }
 
