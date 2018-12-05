715556f7479a951f78f8d46205e5eae7d6bbed50
Dave Burke <david.burke@securian.com>
2018-12-05 07:15:20 -0600

    Implement Day05 Part1

--- src/main/kotlin/aoc/day05/Day05.kt
+++ src/main/kotlin/aoc/day05/Day05.kt
@@ -4,8 +4,25 @@ import aoc.Day
 
 object Day05: Day {
 
+	fun doFold(acc: String, c: Char): String {
+			if(acc.isEmpty()){
+				return "${c}"
+			}
+			val lastChar = acc.last()
+			if(lastChar.equals(c, ignoreCase = true)){
+				if(lastChar.isUpperCase() != c.isUpperCase()){
+					return acc.dropLast(1)
+				}
+			}
+			return acc + c
+	}
+
+	fun process(polymer: String): String {
+		return polymer.fold("") { acc, c -> doFold(acc, c) }
+	}
+
 	override fun part1(input: List<String>): String {
-		return ""
+		return process(input[0]).length.toString()
 	}
 
 	override fun part2(input: List<String>): String {
--- src/test/kotlin/aoc/day05/Day05Test.kt
+++ src/test/kotlin/aoc/day05/Day05Test.kt
@@ -1,6 +1,7 @@
 package aoc.day05
 
 import aoc.AocTestUtil
+import aoc.inputForDay
 import org.junit.Ignore
 import org.junit.Test
 import kotlin.test.assertEquals
@@ -9,18 +10,40 @@ class Day05Test {
 	private val day = Day05
 	private val util: AocTestUtil = AocTestUtil(day)
 
+
+	private fun testProcess(input: String, expected: String){
+		val result = day.process(input)
+		assertEquals(expected, result)
+	}
+
 	@Test
 	fun testPart1_sample0() {
-		// Given
-		val input = listOf(
-				""
-		)
+		testProcess("aA", "")
+	}
 
-		// When
-		val result = day.part1(input)
+	@Test
+	fun testPart1_sample1() {
+		testProcess("abBA", "")
+	}
 
-		// Then
-		assertEquals("", result)
+	@Test
+	fun testPart1_sample2() {
+		testProcess("abAB", "abAB")
+	}
+
+	@Test
+	fun testPart1_sample3() {
+		testProcess("aabAAB", "aabAAB")
+	}
+
+	@Test
+	fun testPart1_sample4() {
+		testProcess("dabAcCaCBAcCcaDA", "dabCBAcaDA")
+	}
+
+	@Test
+	fun testPart1(){
+		util.testPart1(inputForDay(5), "10972")
 	}
 
 	@Test
