6393ddd2566b1860b5a8e8af53be23e5db86600f
Dave Burke <dburke84@gmail.com>
2018-12-05 20:28:38 -0600

    Add Day06 stubs

---
+++ src/test/kotlin/aoc/day06/Day06Test.kt
@@ -0,0 +1,25 @@
+package aoc.day05
+
+import aoc.AocTestUtil
+import aoc.inputForDay
+import org.junit.Test
+import org.junit.Ignore
+import kotlin.test.assertEquals
+
+class Day06Test {
+
+	private val day = Day06
+	private val util: AocTestUtil = AocTestUtil(day)
+
+	@Test
+	fun testPart1_sample0() {
+		util.testPart1(listOf(""), "")
+	}
+
+	@Test
+	@Ignore
+	fun testPart2_sample0(){
+		util.testPart2(listOf(""), "")
+	}
+
+}
