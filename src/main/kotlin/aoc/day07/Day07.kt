f7ac03ef2fc482911e3027423d1eeb7e7980b622
Dave Burke <dburke84@gmail.com>
2018-12-07 06:27:24 -0600

    Add Day07 stubs

---
+++ src/main/kotlin/aoc/day07/Day07.kt
@@ -0,0 +1,15 @@
+package aoc.day07
+
+import aoc.Day
+
+object Day07: Day {
+
+	override fun part1(input: List<String>): String {
+		return ""
+	}
+
+	override fun part2(input: List<String>): String {
+		TODO("Not unlocked")
+	}
+
+}
