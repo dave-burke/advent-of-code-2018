4ab09f8ad1486317d1df9ef48bd5802f3d38acf9
Dave Burke <dburke84@gmail.com>
2018-12-08 20:29:59 -0600

    Add Day08 stubs

---
+++ src/main/kotlin/aoc/day08/Day08.kt
@@ -0,0 +1,15 @@
+package aoc.day08
+
+import aoc.Day
+
+object Day08: Day {
+
+	override fun part1(input: List<String>): String {
+		TODO("Not started")
+	}
+
+	override fun part2(input: List<String>): String {
+		TODO("Not unlocked yet")
+	}
+
+}
---
+++ src/test/kotlin/aoc/day08/Day08Test.kt
@@ -0,0 +1,39 @@
+package aoc.day08
+
+import aoc.AocTestUtil
+import aoc.inputForDay
+import org.junit.Test
+import org.junit.Ignore
+import kotlin.test.assertEquals
+
+class Day08Test {
+
+	private val day = Day08
+	private val util: AocTestUtil = AocTestUtil(day)
+
+	@Test
+	fun testPart1_sample0() {
+		util.testPart1(listOf(
+		), "TODO")
+	}
+
+	@Test
+	@Ignore
+	fun testPart1() {
+		util.testPart1(inputForDay(8), "TODO")
+	}
+
+	@Test
+	@Ignore
+	fun testPart2_sample0() {
+		util.testPart2(listOf(
+		), "TODO")
+	}
+
+	@Test
+	@Ignore
+	fun testPart2() {
+		util.testPart2(inputForDay(8), "TODO")
+	}
+
+}
